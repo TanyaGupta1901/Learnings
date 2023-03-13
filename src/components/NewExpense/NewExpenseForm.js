6ef4b1d44679a74eaa28e96f57e3e0c5f8c990f1
TanyaGupta1901 <tanyag@tekion.com>
2023-03-13 14:48:18 +0530

    add expense button added

--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -1,26 +1,33 @@
-import React from 'react'
-import Card from '../UI/Card'
-import "./NewExpense.css"
+import React from "react";
+import Card from "../UI/Card";
+import "./NewExpense.css";
 
 function NewExpenseForm() {
   return (
     <Card className="new-expense">
-      <form className='expense-form'>
+      <form className="expense-form">
         <div>
-        <label> Title </label>
-        <input placeholder='please enter item name' name='title'></input>
+          <label> Title </label>
+          <input placeholder="please enter item name" name="title"></input>
         </div>
         <div>
-        <label>Price</label>
-        <input type='number' placeholder='please enter item price' name='price'></input>
+          <label>Price</label>
+          <input
+            type="number"
+            placeholder="please enter item price"
+            name="price"
+          ></input>
         </div>
         <div>
-        <label>Date</label>
-        <input type='date' placeholder='dd-mm-yyyy'></input>
+          <label>Date</label>
+          <input type="date" placeholder="dd-mm-yyyy"></input>
         </div>
       </form>
+      <div className="new-expense-button">
+          <button  >Add Expense</button>
+        </div>
     </Card>
-  )
+  );
 }
 
-export default NewExpenseForm
+export default NewExpenseForm;
