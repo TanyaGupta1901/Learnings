51d9ba6f6e6c983be7fc2a7b9d6ca38e8b3ec775
TanyaGupta1901 <tanyag@tekion.com>
2023-03-16 16:54:28 +0530

    base-template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Expense tracker</h1>
+        <h1>Yor app</h1>
       </header>
     </div>
   );
--- src/components/NewExpense/NewExpenseForm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NewExpenseForm() {
-  return (
-    <div>NewExpenseForm</div>
-  )
-}
-
-export default NewExpenseForm
